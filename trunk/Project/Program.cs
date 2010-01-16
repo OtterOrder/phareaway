4bfbe2e1bac1e19ff4bd425b2a5d35a746e90126
germain.mazac <germain.mazac@611b2730-02cb-11df-afd9-7791053e64b7>
2010-01-16 18:33:58 +0000

    Test code

---
+++ trunk/Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PhareAway
+{
+    class Program
+    {
+        /// The main entry point for the application.
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
