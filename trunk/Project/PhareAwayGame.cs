9b3c1f2303ae475f8308a6f0e93c71226b421b92
germain.mazac <germain.mazac@611b2730-02cb-11df-afd9-7791053e64b7>
2010-01-17 14:05:09 +0000

    Launch empty window.

---
+++ trunk/Project/PhareAwayGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Media;
+
+namespace PhareAway
+{
+    public class PhareAwayGame : Microsoft.Xna.Framework.Game
+    {
+
+        private GraphicsDeviceManager graphics;
+
+        private const int BackBufferWidth = 1024;
+        private const int BackBufferHeight = 768;
+
+        public PhareAwayGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = BackBufferWidth;
+            graphics.PreferredBackBufferHeight = BackBufferHeight;
+        }
+    }
+}
