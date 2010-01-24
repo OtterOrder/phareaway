48eb11ec0c6a352b02d06b9a728c8a1bec3ab1cb
alban.chagnoleau <alban.chagnoleau@611b2730-02cb-11df-afd9-7791053e64b7>
2010-01-24 17:40:35 +0000

    Création d'un dossier Src Ajout du CollisionsManager et gestion des collisions à l'aide de BoundingBox

--- Project/Src/Level.cs
+++ Project/Src/Level.cs
@@ -35,8 +35,9 @@ namespace PhareAway
 
             Spr = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Inside/Characters/Archi/Archi_Walk", Content, 4, 15);
             Spr.Depth = 0.06f;
-            //Spr._mPosition.X = 205.42f;
-            //Spr._mPosition.Y = 105.42f;
+            Spr._mPosition.X = 205.42f;
+            Spr._mPosition.Y = 105.42f;
+            Spr.SetBoundingBox(0, new Vector2(0.0f, 0.0f), new Vector2(Spr.Width, Spr.Height));
             if (Spr.AnimPlayer != null)
             {
                 Spr.AnimPlayer.Loop = true;
@@ -45,6 +46,7 @@ namespace PhareAway
 
             Spr2 = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Inside/Characters/Archi/Archi_Walk", Content);
             Spr2.Depth = 0.07f;
+            Spr2.SetBoundingBox(1, new Vector2(0.0f, 0.0f), new Vector2(Spr2.Width, Spr2.Height));
         }
 
         public void Update(float _Dt)
@@ -59,26 +61,46 @@ namespace PhareAway
             if (CurKeyboardState.IsKeyDown(Keys.Right) /*&& LastKeyboardState.IsKeyUp(Keys.Right)*/)
             {
                 Spr._mPosition.X += Speed;
-                Play = true;
+
+                BoundingBox Collision = CollisionsManager.Singleton.Collide(Spr, 1);
+                if (Collision != null)
+                    Spr._mPosition.X -= Speed;
+                else
+                    Play = true;
             }
             else
             if (CurKeyboardState.IsKeyDown(Keys.Left) /*&& LastKeyboardState.IsKeyUp(Keys.Left)*/)
             {
                 Spr._mPosition.X -= Speed;
-                Play = true;
+
+                BoundingBox Collision = CollisionsManager.Singleton.Collide(Spr, 1);
+                if (Collision != null)
+                    Spr._mPosition.X += Speed;
+                else
+                    Play = true;
             }
 
 
             if (CurKeyboardState.IsKeyDown(Keys.Up))
             {
                 Spr._mPosition.Y -= Speed;
-                Play = true;
+
+                BoundingBox Collision = CollisionsManager.Singleton.Collide(Spr, 1);
+                if (Collision != null)
+                    Spr._mPosition.Y += Speed;
+                else
+                    Play = true;
             }
             else
             if (CurKeyboardState.IsKeyDown(Keys.Down))
             {
                 Spr._mPosition.Y += Speed;
-                Play = true;
+
+                BoundingBox Collision = CollisionsManager.Singleton.Collide(Spr, 1);
+                if (Collision != null)
+                    Spr._mPosition.Y += Speed;
+                else
+                    Play = true;
             }
 
             if (Spr.AnimPlayer != null)
