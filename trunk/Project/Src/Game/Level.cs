efa618b1177860b8555cc403791ac30f2a98db71
alban.chagnoleau <alban.chagnoleau@611b2730-02cb-11df-afd9-7791053e64b7>
2010-02-07 21:37:30 +0000

    Mise en place du deuxième personnage (Philo)

--- trunk/Project/Src/Game/Level.cs
+++ trunk/Project/Src/Game/Level.cs
@@ -14,7 +14,8 @@ namespace PhareAway
     {
         private Background Bg;
         private Sprite Spr, bgDecor;
-        private Character _mArchi;
+
+        private Character _mArchi, _mPhilo;
 
         private UInt32 _mSceneOutside;
         private UInt32 _mSceneInside;
@@ -43,10 +44,11 @@ namespace PhareAway
             _mCamPhilo.SetViewportParam(852, 0, 428, 720);
 
             Bg = SceneManager.Singleton.GetNewBackground("Graphics/Backgrounds/bg_Space_01", Content, _mSceneInside);
-            Bg.Depth = 0.5f;
+            Bg.Depth = 0.9f;
             Bg._mSpeed.Y = -0.05f;
 
             _mArchi = new Character();
+            _mPhilo = new Character();
 
             // Ground
             Spr = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Inside/Collisions/Ground", Content, _mSceneInside);
@@ -69,34 +71,62 @@ namespace PhareAway
 
             //
             bgDecor = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Inside/Decor/Background", Content, _mSceneInside);
-            bgDecor.Depth = 0.4f;
+            bgDecor.Depth = 0.7f;
             bgDecor.mPosition.X = 100.0f;
             bgDecor.mPosition.Y = 100.0f;
 
             //-----------------
-            // Init Player
-            CharacterParameters Params = new CharacterParameters();
-            Params.mFileBase = "Graphics/Sprites/Inside/Characters/Archi/";
+            // Init Players
+            CharacterParameters ArchiParams = new CharacterParameters();
+            ArchiParams.mDepth = 0.4f;
+            ArchiParams.mFileBase = "Graphics/Sprites/Inside/Characters/Archi/";
 
-            Params.mSpritesParams[0].mFileName = "Archi_Idle";
+            ArchiParams.mSpritesParams[0].mFileName = "Archi_Idle";
 
-            Params.mSpritesParams[1].mFileName = "Archi_Walk";
-            Params.mSpritesParams[1].mNbFrames = 4;
-            Params.mSpritesParams[1].mFps = 15.0f;
-            Params.mSpritesParams[1].mLoop = true;
+            ArchiParams.mSpritesParams[1].mFileName = "Archi_Walk";
+            ArchiParams.mSpritesParams[1].mNbFrames = 4;
+            ArchiParams.mSpritesParams[1].mFps = 15.0f;
+            ArchiParams.mSpritesParams[1].mLoop = true;
 
-            Params.mSpritesParams[2].mFileName = "Archi_Jump";
+            ArchiParams.mSpritesParams[2].mFileName = "Archi_Jump";
 
-            Params.mSpritesParams[3].mFileName = "Archi_Fall";
+            ArchiParams.mSpritesParams[3].mFileName = "Archi_Fall";
 
-            _mArchi.Init(Content, Params, _mSceneInside);
+            ArchiParams.mInputParams.mRight = Keys.D;
+            ArchiParams.mInputParams.mLeft  = Keys.Q;
+            ArchiParams.mInputParams.mUp    = Keys.Z;
+
+            _mArchi.Init(Content, ArchiParams, _mSceneInside);
+
+            CharacterParameters PhiloParams = new CharacterParameters();
+            PhiloParams.mDepth = 0.41f;
+            PhiloParams.mFileBase = "Graphics/Sprites/Inside/Characters/Philo/";
+
+            PhiloParams.mSpritesParams[0].mFileName = "Philo_Idle";
+
+            PhiloParams.mSpritesParams[1].mFileName = "Philo_Walk";
+            PhiloParams.mSpritesParams[1].mNbFrames = 4;
+            PhiloParams.mSpritesParams[1].mFps = 15.0f;
+            PhiloParams.mSpritesParams[1].mLoop = true;
+
+            PhiloParams.mSpritesParams[2].mFileName = "Philo_Jump";
+
+            PhiloParams.mSpritesParams[3].mFileName = "Philo_Fall";
+
+            PhiloParams.mInputParams.mRight = Keys.M;
+            PhiloParams.mInputParams.mLeft  = Keys.K;
+            PhiloParams.mInputParams.mUp    = Keys.O;
+
+            _mPhilo.Init(Content, PhiloParams, _mSceneInside);
         }
 
         public void Update(float _Dt)
         {
             _mArchi.Update(_Dt);
+            _mPhilo.Update(_Dt);
+
             _mCamArchi.mFocus = _mArchi.GetPosition();
-            _mCamPhilo.mFocus = _mArchi.GetPosition();
+            _mCamPhilo.mFocus = _mPhilo.GetPosition();
         }
     }
 }
