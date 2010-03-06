85051736375cd08e5cf3e9370fabdc030b8dd5b6
alban.chagnoleau <alban.chagnoleau@611b2730-02cb-11df-afd9-7791053e64b7>
2010-03-06 00:28:51 +0000

    Ajout de fichiers

---
+++ Project/Src/Utilities/Obstacle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace PhareAway
+{
+    //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    public class Obstacle
+    {
+        private Vector2 _mPosition = new Vector2();
+        private float   _mSpeed = 0.0f;
+
+        private Sprite _mSprite = null;
+
+        //-------------------------------------------------------------------------
+        public Obstacle()
+        {
+        }
+
+        //-------------------------------------------------------------------------
+        public void Init(ContentManager _ContentManager, string _FileName, UInt32 _SceneId, float _Depth, float _Speed, float _X, float _Y)
+        {
+            _mSprite = SceneManager.Singleton.GetNewSprite( _FileName,
+                                                            _ContentManager,
+                                                            _SceneId);
+            _mSprite.Depth = _Depth;
+            _mPosition = new Vector2(_X, _Y);
+            _mSprite.mOrigin = new Vector2((float)_mSprite.Width / 2.0f, (float)_mSprite.Height / 2.0f);
+            _mSprite.SetBoundingBox(0, new Vector2(0.0f, 0.0f), new Vector2(_mSprite.Width, _mSprite.Height));
+            _mSprite.mVisible = true;
+
+            _mSpeed = _Speed;
+        }
+
+        //------------------------------------------------------------------
+        public Vector2 GetPosition() { return _mPosition; }
+
+        public void SetPosition(Vector2 _Position) { _mPosition = _Position; }
+
+        private BoundingBox BBox
+        {
+            get
+            {
+                _mSprite.GetBoundingBox().Update();
+                return _mSprite.GetBoundingBox();
+            }
+        }
+
+        public bool Visible
+        {
+            get { return _mSprite.mVisible; }
+            set { _mSprite.mVisible = value; }
+        }
+        //-------------------------------------------------------------------------
+        //-------------------------------------------------------------------------
+        public void Update(float _Dt)  // MilliSeconds
+        {
+            _mPosition.Y += _mSpeed*_Dt;
+
+            _mSprite.mPosition = _mPosition;
+        }
+    }
+}
---
+++ Project/Src/Utilities/ObstacleManager.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace PhareAway
+{
+    //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    public class ObstaclesParameters
+    {
+        public string mFileBase = "";
+        public string[] mSprites = null;
+        private int _mNbObstacles = 1;
+        private int _mNbSprites = 1;
+        public float mDepth = 0.5f;
+
+        public float mSpeedA = 1.0f;
+        public float mSpeedB = 0.0f;
+
+        public float mObstacleA = 1.0f;
+        public float mObstacleB = 0.0f;
+
+        public float mStartTimeAMin = 0.0f;
+        public float mStartTimeAMax = 0.0f;
+        public float mStartTimeB = 0.0f;
+
+        public float mEndTimeAMin = 0.0f;
+        public float mEndTimeAMax = 0.0f;
+        public float mEndTimeB = 0.0f;
+
+        public Vector2 mXRange = new Vector2(0.0f, 100.0f);
+        public float mYInit = 0.0f;
+
+        public float mYMax = 100.0f;
+
+        public ObstaclesParameters(UInt32 _NbObstacles, UInt32 _NbSprites)
+        {
+            _mNbObstacles = (int)_NbObstacles;
+            _mNbSprites = (int)_NbSprites;
+            mSprites = new string[_NbSprites];
+        }
+
+        public int NbObstacles
+        {
+            get { return _mNbObstacles; }
+        }
+
+        public int NbSprites
+        {
+            get { return _mNbSprites; }
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    public class ObstacleManager
+    {
+        public enum State
+        {
+            Wait = 0,
+            LaunchObstacles = 1
+        }
+
+        private static ObstacleManager _mSingleton = null;
+
+        private List<Obstacle>  _mObstacleList = null;
+        private int             _mNbActiveObstacles = 0;
+
+        private ObstaclesParameters _mParams;
+        private Random _mRand = new Random();
+
+        private State _mState = State.LaunchObstacles;
+
+        private float _mTime = 0.0f;
+        private float _mNextStateTime;
+
+        private ContentManager _mContentManager = null;
+        private UInt32 _mSceneId;
+
+        //-------------------------------------------------------------------------
+        private ObstacleManager()
+        {
+        }
+
+        public void Initialize (ObstaclesParameters _Params, ContentManager _ContentManager, UInt32 _SceneId)
+        {
+            _mParams = _Params;
+
+            _mObstacleList = new List<Obstacle>(_mParams.NbObstacles);
+            _mNbActiveObstacles = 0;
+
+            for (int i = 0; i < _mParams.NbObstacles; i++)
+                _mObstacleList.Add(new Obstacle());
+
+            _mContentManager = _ContentManager;
+            _mSceneId = _SceneId;
+
+            ChangeState(State.Wait);
+        }
+
+        //-------------------------------------------------------------------------
+        public static ObstacleManager Singleton
+        {
+            get
+            {
+                if (_mSingleton == null)
+                    _mSingleton = new ObstacleManager();
+
+                return _mSingleton;
+            }
+        }
+
+        //-------------------------------------------------------------------------
+        //-------------------------------------------------------------------------
+        public void Update(float _Dt)
+        {
+            _mTime += _Dt/1000.0f;
+
+            if (_mTime >= _mNextStateTime)
+            {
+                switch (_mState)
+                {
+                    case State.Wait:
+                        ChangeState(State.LaunchObstacles);
+                        break;
+
+                    case State.LaunchObstacles:
+                        ChangeState(State.Wait);
+                        break;
+                }
+            }
+
+            switch (_mState)
+            {
+                case State.Wait:
+                    break;
+
+                case State.LaunchObstacles:
+                    UpdateLaunchObstacles(_Dt);
+                    break;
+            }
+
+            for (int i = _mNbActiveObstacles - 1; i >= 0; i--)
+            {
+                _mObstacleList[i].Update(_Dt);
+
+                if (_mObstacleList[i].GetPosition().Y >= _mParams.mYMax)
+                    FreeObstacle(i);
+            }
+        }
+
+        //-----------------------------------
+        private void ChangeState (State _State)
+        {
+            if (_State == _mState)
+                return;
+
+            switch (_State)
+            {
+                case State.Wait:
+                    _mNextStateTime = (float)_mRand.NextDouble() * (_mParams.mStartTimeAMax - _mParams.mStartTimeAMin) + _mParams.mStartTimeAMin + _mParams.mStartTimeB /* * Time */;
+                    break;
+
+                case State.LaunchObstacles:
+                    _mNextStateTime = (float)_mRand.NextDouble() * (_mParams.mEndTimeAMax - _mParams.mEndTimeAMin) + _mParams.mEndTimeAMin + _mParams.mEndTimeB /* * Time */;
+                    break;
+            }
+
+            _mTime = 0.0f;
+            _mState = _State;
+        }
+
+        //-----------------------------------
+        private void UpdateLaunchObstacles(float _Dt)
+        {
+            if (_mNbActiveObstacles < _mParams.NbObstacles)
+            {
+                _mObstacleList[_mNbActiveObstacles].Init(   _mContentManager,
+                                                            _mParams.mFileBase + _mParams.mSprites[_mRand.Next(0, _mParams.NbSprites-1)],
+                                                            _mSceneId,
+                                                            _mParams.mDepth,
+                                                            _mParams.mSpeedA,
+                                                            _mParams.mXRange.X + (_mParams.mXRange.Y - _mParams.mXRange.X) * (float)_mRand.NextDouble(),
+                                                            _mParams.mYInit);
+                _mNbActiveObstacles++;
+            }
+        }
+
+        //-----------------------------------
+        private void FreeObstacle(int _Idx)
+        {
+            Obstacle obs = _mObstacleList[_Idx];
+            _mObstacleList.RemoveAt(_Idx);
+            _mObstacleList.Add(obs);
+            obs.Visible = false;
+
+            _mNbActiveObstacles--;
+        }
+    }
+}
