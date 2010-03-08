99cf5717eb4639af22d351e9f60982f98332be09
germain.mazac <germain.mazac@611b2730-02cb-11df-afd9-7791053e64b7>
2010-03-08 23:28:22 +0000

    Ajout des portes

--- trunk/Project/Src/Game/Gate/GateManager.cs
+++ trunk/Project/Src/Game/Gate/GateManager.cs
@@ -11,23 +11,45 @@ namespace PhareAway
 {
     public class GateManager
     {
+        private static GateManager _mSingleton = null;
 
         private List<Gate> _mGateList = new List<Gate>();
+
+        public static GateManager Singleton
+        {
+            get
+            {
+                if (_mSingleton == null)
+                    _mSingleton = new GateManager();
+
+                return _mSingleton;
+            }
+        }
 
         public GateManager()
         {
 
         }
 
+        //-------------------------------------------------------------------------
         public void AddGate(string _GateSprite, ContentManager _ContentManager, UInt32 _SceneId, Vector2 _Entrance, Vector2 _Exit)
         {
             Gate gate = new Gate(_GateSprite, _ContentManager, _SceneId, _Entrance, _Exit);
+            _mGateList.Add(gate);
 
         }
 
-        public Gate GetGateByPosition()
+        public Vector2 GetExitGateByPosition(Vector2 _GatePosition)
         {
-            return null;
+            foreach (Gate gate in _mGateList)
+            {
+                Vector2 pos = gate.GetGateByEntry(_GatePosition);
+                if (pos != Vector2.Zero)
+                {
+                    return gate.GetExitByEntry(pos);
+                }
+            }
+            return _GatePosition;
         }
 
     }
