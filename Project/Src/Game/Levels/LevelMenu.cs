8a4c0adfa5a212e17a6143d212af0eadba8d8be2
germain.mazac <germain.mazac@611b2730-02cb-11df-afd9-7791053e64b7>
2010-03-16 15:33:28 +0000

    Ajout niveaux tuto et crédits

--- Project/Src/Game/Levels/LevelMenu.cs
+++ Project/Src/Game/Levels/LevelMenu.cs
@@ -108,11 +108,10 @@ namespace PhareAway
                 switch(_mIDChoice)
                 {
                     case 0: _mGame.ChangeLevel(LevelName.Level_Main); break;
-                    case 1:  break;
-                    case 2:  break;
-                    case 3:  break;
+                    case 1: _mGame.ChangeLevel(LevelName.Level_Tuto); break;
+                    case 2: _mGame.ChangeLevel(LevelName.Level_Credits); break;
+                    case 3: _mGame.Exit(); break;
                 }
-               
             }
         }
 
