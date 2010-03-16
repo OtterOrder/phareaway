33892c30df6ba96ff189fbbd9015cdd84dc98ba9
germain.mazac <germain.mazac@611b2730-02cb-11df-afd9-7791053e64b7>
2010-03-16 15:11:26 +0000

    Navigation menu.

--- Project/Src/Game/Levels/LevelMenu.cs
+++ Project/Src/Game/Levels/LevelMenu.cs
@@ -14,9 +14,13 @@ namespace PhareAway
     {
         private PhareAwayGame _mGame;
         private Sprite _mBgMenu;
+        private Sprite [] _mMenuSprite;
+        private Sprite [] _mMenuSpriteSelect;
 
         private UInt32 _mSceneMenu;
         private Camera _mDefaultCam;
+
+        private int _mIDChoice = 0;
 
         public LevelMenu(PhareAwayGame _Game, ContentManager _Content) : base(_Game, _Content)
         {
@@ -31,18 +35,85 @@ namespace PhareAway
             _mDefaultCam.Position = new Vector2(0.0f, 0.0f);
 
             _mBgMenu = SceneManager.Singleton.GetNewSprite("Graphics/Backgrounds/bg_Menu", _mContent, _mSceneMenu);
-            _mBgMenu.mPosition = new Vector2(0.0f, 0.0f);
+            _mBgMenu.mPosition = new Vector2(128.0f, 0.0f);
             _mBgMenu.mOrigin = new Vector2(0.0f, 0.0f);
 
+            _mMenuSprite = new Sprite[4];
+
+            _mMenuSprite[0] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/NewGame_0", _mContent, _mSceneMenu);
+            _mMenuSprite[0].mPosition = new Vector2(1000.0f, 260.0f);
+            _mMenuSprite[0].mOrigin = new Vector2(_mMenuSprite[0].Width, _mMenuSprite[0].Height);
+
+            _mMenuSprite[1] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/HowToPlay_0", _mContent, _mSceneMenu);
+            _mMenuSprite[1].mPosition = new Vector2(1000.0f, 310.0f);
+            _mMenuSprite[1].mOrigin = new Vector2(_mMenuSprite[1].Width, _mMenuSprite[1].Height);
+
+            _mMenuSprite[2] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/Credits_0", _mContent, _mSceneMenu);
+            _mMenuSprite[2].mPosition = new Vector2(1000.0f, 346.0f);
+            _mMenuSprite[2].mOrigin = new Vector2(_mMenuSprite[2].Width, _mMenuSprite[2].Height);
+
+            _mMenuSprite[3] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/Exit_0", _mContent, _mSceneMenu);
+            _mMenuSprite[3].mPosition = new Vector2(1000.0f, 386.0f);
+            _mMenuSprite[3].mOrigin = new Vector2(_mMenuSprite[3].Width, _mMenuSprite[3].Height);
+
+            _mMenuSpriteSelect = new Sprite[4];
+
+            _mMenuSpriteSelect[0] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/NewGame_1", _mContent, _mSceneMenu);
+            _mMenuSpriteSelect[0].mPosition = new Vector2(1000.0f, 260.0f);
+            _mMenuSpriteSelect[0].mOrigin = new Vector2(_mMenuSpriteSelect[0].Width, _mMenuSpriteSelect[0].Height);
+            _mMenuSpriteSelect[0].mVisible = false;
+
+            _mMenuSpriteSelect[1] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/HowToPlay_1", _mContent, _mSceneMenu);
+            _mMenuSpriteSelect[1].mPosition = new Vector2(1000.0f, 310.0f);
+            _mMenuSpriteSelect[1].mOrigin = new Vector2(_mMenuSpriteSelect[1].Width, _mMenuSpriteSelect[1].Height);
+            _mMenuSpriteSelect[1].mVisible = false;
+
+            _mMenuSpriteSelect[2] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/Credits_1", _mContent, _mSceneMenu);
+            _mMenuSpriteSelect[2].mPosition = new Vector2(1000.0f, 346.0f);
+            _mMenuSpriteSelect[2].mOrigin = new Vector2(_mMenuSpriteSelect[2].Width, _mMenuSpriteSelect[2].Height);
+            _mMenuSpriteSelect[2].mVisible = false;
+
+            _mMenuSpriteSelect[3] = SceneManager.Singleton.GetNewSprite("Graphics/Sprites/Menu/Exit_1", _mContent, _mSceneMenu);
+            _mMenuSpriteSelect[3].mPosition = new Vector2(1000.0f, 386.0f);
+            _mMenuSpriteSelect[3].mOrigin = new Vector2(_mMenuSpriteSelect[3].Width, _mMenuSpriteSelect[3].Height);
+            _mMenuSpriteSelect[3].mVisible = false;
         }
 
         public override void Update(float _Dt)
         {
-            if (InputManager.Singleton.IsKeyJustPressed(Keys.Space) || InputManager.Singleton.IsKeyJustPressed(Keys.Escape))
+            _mMenuSprite[_mIDChoice].mVisible = false;
+            _mMenuSpriteSelect[_mIDChoice].mVisible = true;
+
+            if (InputManager.Singleton.IsKeyJustPressed(Keys.S))
             {
-                _mGame.ChangeLevel(LevelName.Level_Main);
+                if (_mIDChoice < 3)
+                {
+                    _mMenuSprite[_mIDChoice].mVisible = true;
+                    _mMenuSpriteSelect[_mIDChoice].mVisible = false;
+                    _mIDChoice++;
+                }
+            }
+            if (InputManager.Singleton.IsKeyJustPressed(Keys.Z))
+            {
+                if (_mIDChoice > 0)
+                {
+                    _mMenuSprite[_mIDChoice].mVisible = true;
+                    _mMenuSpriteSelect[_mIDChoice].mVisible = false;
+                    _mIDChoice--;
+                }
             }
 
+            if (InputManager.Singleton.IsKeyJustPressed(Keys.Enter))
+            {
+                switch(_mIDChoice)
+                {
+                    case 0: _mGame.ChangeLevel(LevelName.Level_Main); break;
+                    case 1:  break;
+                    case 2:  break;
+                    case 3:  break;
+                }
+               
+            }
         }
 
         public override void Draw(SpriteBatch _SprBatch, GraphicsDeviceManager _GraphicsManager)
